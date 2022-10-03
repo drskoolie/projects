cac476e3c0fc947c59c92bdbbebbe33003b3f68b
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-10-03 17:20:49 -0400

    Almost done

--- sts/qfunc.py
+++ sts/qfunc.py
@@ -1,6 +1,6 @@
+## Part 0: Initialization
 import numpy as np
 from statistics import NormalDist
-from matplotlib import pyplot as plt
 
 x = np.arange(0, 5, 0.05)
 y = np.zeros(len(x))
@@ -8,6 +8,52 @@ y = np.zeros(len(x))
 for idx, value in enumerate(x):
     y[idx] = 1 - NormalDist(mu=0, sigma=1).cdf(value)
 
-plt.plot(x, y)
-plt.title("Q function")
-plt.show()
+## Part 1: Latex Document SetUp
+rows = 20
+cols = 5
+
+string = r"""\documentclass{article}
+\usepackage{amsmath}
+
+\begin{document}
+\thispagestyle{empty}
+
+\begin{center}
+  {\small\textbf{Values of Q(x) for 0 $\le$ x $\le$ 5}}
+\end{center}
+
+\begin{center}
+"""
+
+string += r"\begin{tabular}{|"
+string += "cc||" * cols
+string = string[:-1]
+
+string += "}\n\n\t\hline\n"
+
+
+string += "\t "
+for col in range(cols):
+    string += "$Q$ & $Q(x)$ & "
+
+string = string[:-2]
+string += "\\\ \hline \n"
+
+for row in range(rows):
+    string += "\t "
+    for col in range(cols):
+        string += f"{x[col + row*cols]:.2f} & "
+
+    string = string[:-2]
+    string += " \\\ \n"
+
+string += r"""
+\end{tabular}
+\end{center}
+
+\end{document}
+"""
+
+print(string)
+with open("qfunc.tex", "w") as f:
+    f.write(string)
