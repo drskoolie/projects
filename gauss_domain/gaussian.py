230e4ef8dca57d89da1ef3ba062cdf2bc1d88681
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-10-11 14:22:29 -0400

    Added numpy capabilities

--- gauss_domain/gaussian.py
+++ gauss_domain/gaussian.py
@@ -1,19 +1,3 @@
-## Part 0: Intialization
-import numpy as np
-
-
-x0 = np.array([1, 2, 3, 4, 5])
-x1 = np.array([6, 7, 8, 9, 11])
-
-x2 = np.concatenate((x0, x1))
-
-x0.mean()
-x1.mean()
-
-x2.mean()
-
-(x0.mean() + x1.mean())/2
-
 ## Part 1: Building Class
 class Gaussian():
     def __init__(self, mu, var, n):
@@ -21,11 +5,27 @@ class Gaussian():
         self.var = var
         self.n = n
 
+    @classmethod
+    def from_numpy(cls, array):
+        return cls(mu = array.mean(), var = array.var(), n = array.size)
+
     def __repr__(self):
         return f"Gaussian({self.mu:.2f}, {self.var:.2f}, {self.n})"
 
 
-g0 = Gaussian(x0.mean(), x0.std()**2, x0.size)
-g1 = Gaussian(x1.mean(), x1.std()**2, x1.size)
 
-g1.__repr__()
+if __name__ == "__main__":
+    ## Part 0: Intialization
+    import numpy as np
+
+
+    x0 = np.array([1, 2, 3, 4])
+    x1 = np.array([6, 7, 8, 9, 11, 12, 13])
+
+    x2 = np.concatenate((x0, x1))
+
+    g0 = Gaussian(x0.mean(), x0.std()**2, x0.size)
+    g1 = Gaussian(x1.mean(), x1.std()**2, x1.size)
+
+    g2 = Gaussian.from_numpy(x0)
+
