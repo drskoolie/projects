4a418045a56b27f75f0b20741adbff8939536886
Al-Baraa El-Hag <a.elhag01@gmail.com>
2022-11-12 22:18:06 -0500

    No pipeline set up

--- pipeline/main.py
+++ pipeline/main.py
@@ -15,3 +15,29 @@ seed = 123
 
 df = load_dataset('tips').drop(columns=['tip', 'sex']).sample(n=5, random_state=seed)
 df.iloc[[1, 2, 4], [2, 4]] = np.nan
+
+X_train, X_test, y_train, y_test = train_test_split(df.drop(columns=['total_bill', 'size']),
+                                                    df['total_bill'],
+                                                    test_size=0.2,
+                                                    random_state=seed)
+
+## Part 1: No Pipeline
+imputer = SimpleImputer(strategy='constant', fill_value='missing')
+X_train_imputed = imputer.fit_transform(X_train)
+
+encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+X_train_encoder = encoder.fit_transform(X_train_imputed)
+X_train_encoder
+
+print("******************** Training Data ********************")
+display(X_train)
+display(pd.DataFrame(X_train_imputed, columns=X_train.columns))
+display(pd.DataFrame(X_train_encoder, columns=encoder.get_feature_names_out(X_train.columns)))
+
+X_test_imputed = imputer.transform(X_test)
+X_test_encoded = encoder.transform(X_test_imputed)
+
+print("******************** Testing Data ********************")
+display(X_test)
+display(pd.DataFrame(X_test_imputed, columns=X_train.columns))
+display(pd.DataFrame(X_test_encoded, columns=encoder.get_feature_names_out(X_train.columns)))
